3a52d6e6fe64520fc249ff0ace6b562ecd4f09c0
angela <awesomenessknee@gmail.com>
2025-05-14 08:16:18 -0400

    experimenting with webpack

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import "../css/main.scss"; // loads all css
+import { doExample } from "./page_js/script.js"; // loads the function doExample() from example.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = "It works!";
+  doExample();
+});
